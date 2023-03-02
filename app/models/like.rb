c4d65fa4440d211f4f94abce83c92563523076dd
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-03-02 15:41:14 +0300

    Add models

---
+++ app/models/like.rb
@@ -0,0 +1,8 @@
+class Like < ApplicationRecord
+    belongs_to :post
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+    def updates_likes_counter
+        post.update(likes_counter: post.likes_counter+1)
+    end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+    has_many :comment
+    has_many :like
+
+    def updates_posts_counter
+        author.update(post_counter: author.post_counter+1)
+    end
+
+    def recent_five_comments
+        comment.order(created_at: :desc).limit(5)
+    end
+end
