d46ca7e4b2567d5f50281937c6e5d4812ac4e166
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-03-03 15:35:36 +0300

    remove blank space from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :post_counter, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
-
-
   has_many :like, foreign_key: 'author_id'
   has_many :post, foreign_key: 'author_id'
   has_many :comment, foreign_key: 'author_id'
