e0afc131c09950b22168103e06f57bd1893ba160
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-03-06 17:23:12 +0300

    creating the controllers

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,10 @@
+class UserController < ApplicationController
+    
+    def index
+        @users = User.all
+    end
+    def show 
+        @user = User.find(params[:id])
+        @posts = Post.where(author_id: params[:id])
+    end
+end
