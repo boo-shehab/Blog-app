89e467fdd37b1ccd14a43553340b0b048ea52188
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-03-09 11:23:37 +0300

    add view

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,9 +1,11 @@
 class PostController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: @user.id)
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
     @comments = Comment.where(post_id: params[:id])
   end
