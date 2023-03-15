5b3deba5aaa7c1a945d2203f5550cd940ccf903f
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-03-15 22:14:00 +0300

    log in and log out features

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,18 @@
+class PasswordsController < Devise::PasswordsController
+    def new
+      super
+    end
+  
+    def create
+      super
+    end
+  
+    def edit
+      super
+    end
+  
+    def update
+      super
+    end
+  end
+  
